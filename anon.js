61e0c0488b4ea337d21f2d983a0c4f17696cc9e7
LucasZamprogno <lucaszamprogno@gmail.com>
2018-10-30 02:31:39 -0700

    Attempt to make replacement more robust

--- anon.js
+++ anon.js
@@ -3,12 +3,16 @@ let debounceTimeout = null;
 const MUTATION_TIMEOUT = 150; // ms
 
 const main = document.querySelector("div.main_panel");
-const observer = new MutationObserver(DOMDebounce);
+const observer = new MutationObserver(function (mutations) {
+    if(!selfMutations(mutations)) {
+        setupAnonify();
+    }
+});
 observer.observe(main, {subtree: true, childList: true});
 
 // Original use was https://github.com/LucasZamprogno/GitHub-Monitor/blob/master/contentScript.js
 // Reduce multiple firings of anonify. Still often runs 2-3 times
-function DOMDebounce() {
+function setupAnonify() {
     if(debounceTimeout === null) { // First DOM change in a while? Set the timer
         debounceTimeout = setTimeout(function(){
             anonify();
@@ -28,14 +32,17 @@ function DOMDebounce() {
 function anonify() {
     const nameNumMap = {}; // Map of users to their anonymous number
     let anonCount = 0;
-	const nameArray = document.querySelectorAll("div.user_name"); // Get all usernames
+    const nameArray = main.querySelectorAll("div.user_name"); // Get all usernames
+    for (const nameElem of nameArray) {
+        if (nameElem.textContent.trim() === "Loading...") {
+            setupAnonify(); // Try again later
+            return;
+        }
+    }
 	for (const nameElem of nameArray) {
 	    try {
             if (nameElem.getAttribute("anon") === "stud") { // If anonymous to students
-                // "anon to classmates" span can be sibling or child for some reason, get via parent and remove
-                nameElem.parentNode.querySelector("span.smallText").remove();
                 const name = nameElem.textContent.trim();
-                nameElem.title = name; // Show name on hover
                 // Get/set their anonymous number
                 let num;
                 if (nameNumMap[name]) {
@@ -45,13 +52,28 @@ function anonify() {
                     nameNumMap[name] = anonCount;
                     num = anonCount;
                 }
-                nameElem.innerHTML = "Anonymous #" + num; // Replace name
                 nameElem.setAttribute("anon", "extra"); // Won't get re-anonymized after more DOM mutations
+                // "anon to classmates" span can be sibling or child for some reason, get via parent and remove
+                nameElem.parentNode.querySelector("span.smallText").remove();
+                nameElem.title = name; // Show name on hover
+                nameElem.innerHTML = "Anonymous #" + num; // Replace name
             }
         } catch (e) {
 	        // Just in case something got unexpectedly modified/removed (likely poor debounce timing)
         }
     }
+}
+
+function selfMutations(mutations) {
+    for (const mutation of mutations) {
+        if (mutation.target.classList.contains("user_name") &&
+            mutation.removedNodes.length > 0 &&
+            (mutation.removedNodes[0].innerText === "(anon. to classmates)" ||
+            mutation.addedNodes[0].parentNode.getAttribute("anon") === "extra")) {
+            return true;
+        }
+    }
+    return false;
 }
 
 console.log("Extra-anonymous loaded");
